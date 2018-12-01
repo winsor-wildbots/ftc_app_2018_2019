6ba974e2191da77919252a4fb792c13a16379896
cmccurley22 <cmccurley@winsor.edu>
2018-11-30 19:36:08 -0500

    Added new driving op mode

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleOpMode2.java
@@ -0,0 +1,41 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+
+import org.firstinspires.ftc.enums.DrivingMode;
+import org.firstinspires.ftc.libraries.DrivingLibrary;
+
+@TeleOp
+public class TeleOpMode2 extends LinearOpMode {
+    DrivingLibrary drivingLibrary;
+    int drivingMode;
+
+    public void runOpMode() throws InterruptedException {
+        drivingLibrary = new DrivingLibrary(this);
+        drivingLibrary.setSpeed(1);
+        drivingMode = 0;
+        drivingLibrary.setMode(drivingMode);
+
+        telemetry.addData("Status", "Initialized");
+        telemetry.update();
+
+        waitForStart();
+
+        while (opModeIsActive()) {
+            if (gamepad1.b) {
+                drivingMode++;
+                drivingMode %= DrivingMode.values().length;
+                drivingLibrary.setMode(drivingMode);
+            }
+
+            drivingLibrary.driveStraight(gamepad1.left_stick_x, -gamepad1.left_stick_y);
+            drivingLibrary.turn(gamepad1.right_stick_x, -gamepad1.right_stick_y);
+
+            telemetry.addData("Status", "Running");
+            telemetry.addData("Brake Mode", drivingLibrary.getMode());
+
+            telemetry.update();
+        }
+    }
+}
